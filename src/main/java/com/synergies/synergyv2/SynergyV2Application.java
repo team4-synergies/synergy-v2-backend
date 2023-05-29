00b1f9c7c93dacd72d490fa401cbe0e26165f3e5
yonguk929@gmail.com <yonguk929@gmail.com>
2023-05-29 20:01:55 +0900

    init : 프로젝트 설정

---
+++ src/main/java/com/synergies/synergyv2/SynergyV2Application.java
@@ -0,0 +1,13 @@
+package com.synergies.synergyv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynergyV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/synergies/synergyv2/SynergyRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synergies.synergyv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyRenewalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
