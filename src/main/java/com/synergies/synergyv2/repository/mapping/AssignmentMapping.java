d0a7fe8733ee053802c0df8bf0cfa35c748c0454
yewonkang00 <yewon00000@naver.com>
2023-05-30 23:58:41 +0900

    [SY-115] Add : 과제 Mapping Intercepter

---
+++ src/main/java/com/synergies/synergyv2/repository/mapping/AssignmentMapping.java
@@ -0,0 +1,9 @@
+package com.synergies.synergyv2.repository.mapping;
+
+import java.time.LocalDateTime;
+
+public interface AssignmentMapping {
+    Integer getId();
+    String getTitle();
+    LocalDateTime getRegDate();
+}
