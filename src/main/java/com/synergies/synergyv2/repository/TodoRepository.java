76009ad88255a3e8c5bcced6bd8d6a08701bdee2
changheonkim <heon5652@gmail.com>
2023-06-02 09:45:03 +0900

    [SY-129] Update : Repository 어노테이션 추가

--- src/main/java/com/synergies/synergyv2/repository/TodoRepository.java
+++ src/main/java/com/synergies/synergyv2/repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.synergies.synergyv2.repository;
 
 import com.synergies.synergyv2.model.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Integer> {
 }
