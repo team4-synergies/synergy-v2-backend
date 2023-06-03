df3519107501fb6a677ba93de0968c91cf365869
yonguk929@gmail.com <yonguk929@gmail.com>
2023-06-03 17:33:21 +0900

    Fix : UserRepository id 수정

--- src/main/java/com/synergies/synergyv2/repository/UserRepository.java
+++ src/main/java/com/synergies/synergyv2/repository/UserRepository.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     List<UserMapping> findAllProjectedBy();
 
 
