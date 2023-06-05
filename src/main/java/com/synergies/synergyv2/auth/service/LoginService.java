db7c4d3533c796a63bd974654ecb2c04a348f71b
yonguk929@gmail.com <yonguk929@gmail.com>
2023-06-05 16:47:34 +0900

    Update : 카카오 유저 정보 획득 후 카카오 accessToken 만료

--- src/main/java/com/synergies/synergyv2/auth/service/LoginService.java
+++ src/main/java/com/synergies/synergyv2/auth/service/LoginService.java
@@ -30,22 +30,24 @@ public class LoginService {
     @Value("${kakao.config.oauth.client_secret}")
     private String kakaoOauthSecret;
 
+    @Value("${kakao.config.oauth.logout_uri}")
+    private String kakaoLogoutUri;
+
     public String getAuthorizationUrl() {
-        String kakaoAuthrizationUrl = "https://kauth.kakao.com/oauth/authorize?"
+        return "https://kauth.kakao.com/oauth/authorize?"
                 + "client_id=" + kakaoOauthClientId
                 + "&redirect_uri=" + kakaoRedirectUri
                 + "&response_type=code";
-        return kakaoAuthrizationUrl;
     }
 
-    public KakaoTokenInfoDto getToken(String authorize_code) throws IOException {
+    public KakaoTokenInfoDto getToken(String authorizeCode) throws IOException {
         final String RequestUrl = "https://kauth.kakao.com/oauth/token";
-        final List<NameValuePair> postParams = new ArrayList<NameValuePair>();
+        final List<NameValuePair> postParams = new ArrayList<>();
         postParams.add(new BasicNameValuePair("grant_type", "authorization_code"));
         postParams.add(new BasicNameValuePair("client_id", kakaoOauthClientId)); // REST API KEY
         postParams.add(new BasicNameValuePair("redirect_uri", kakaoRedirectUri)); // 리다이렉트 URI
         postParams.add(new BasicNameValuePair("client_secret", kakaoOauthSecret));
-        postParams.add(new BasicNameValuePair("code", authorize_code)); // 로그인 과정 중 얻은 code 값
+        postParams.add(new BasicNameValuePair("code", authorizeCode)); // 로그인 과정 중 얻은 code 값
 
         HttpClient client = HttpClientBuilder.create().build();
         HttpPost post = new HttpPost(RequestUrl);
@@ -57,8 +59,7 @@ public class LoginService {
 
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonNode = mapper.readTree(response.getEntity().getContent());
-        KakaoTokenInfoDto kakaoAccessTokenDto = mapper.treeToValue(jsonNode, KakaoTokenInfoDto.class);
-        return kakaoAccessTokenDto;
+        return mapper.treeToValue(jsonNode, KakaoTokenInfoDto.class);
     }
 
     public KakaoUserInfoDto getKakaoUserInfo(String accessToken) throws IOException {
@@ -80,12 +81,17 @@ public class LoginService {
         // JSON 형태 반환값 처리
         ObjectMapper mapper = new ObjectMapper();
         returnNode = mapper.readValue(response.getEntity().getContent(), JsonNode.class);
-        KakaoUserInfoDto kakaoUserInfoResponse = KakaoUserInfoDto.builder()
+        return KakaoUserInfoDto.builder()
                 .userKakaoId(String.valueOf(returnNode.get("id")))
                 .userNickname(returnNode.get("kakao_account").get("profile").get("nickname").asText())
                 .email(returnNode.get("kakao_account").get("email").asText())
                 .build();
+    }
 
-        return kakaoUserInfoResponse;
+    public void expiredKakaoAccessToken(String accessToken) throws IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost expiredPost = new HttpPost(kakaoLogoutUri);
+        expiredPost.setHeader("Authorization", "Bearer "  + accessToken);
+        client.execute(expiredPost);
     }
 }
