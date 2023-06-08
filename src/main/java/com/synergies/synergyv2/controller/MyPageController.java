612d4d9f3bbf03484c795f9fd0475da6d09239f8
yonguk929@gmail.com <yonguk929@gmail.com>
2023-06-08 09:29:33 +0900

    [SY-146] Update : 마이페이지 api 구현(get, put)

---
+++ src/main/java/com/synergies/synergyv2/controller/MyPageController.java
@@ -0,0 +1,37 @@
+package com.synergies.synergyv2.controller;
+
+import com.synergies.synergyv2.auth.CustomUserDetails;
+import com.synergies.synergyv2.common.response.CommonResponse;
+import com.synergies.synergyv2.common.response.code.CommonCode;
+import com.synergies.synergyv2.model.dto.MyPageResponseDto;
+import com.synergies.synergyv2.service.MyPageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/mypage")
+public class MyPageController {
+
+    private final MyPageService myPageService;
+
+    @GetMapping
+    public ResponseEntity<CommonResponse> getMyPageInfo(@AuthenticationPrincipal CustomUserDetails userDetails){
+        return ResponseEntity.ok(CommonResponse.toResponse(CommonCode.OK, MyPageResponseDto.builder()
+                .name(userDetails.getName())
+                .email(userDetails.getEmail())
+                .profileImage(userDetails.getProfileImage())
+                .githubEmail(userDetails.getGithubEmail())
+                .build()));
+    }
+
+    @PutMapping
+    public ResponseEntity<CommonResponse> updateGithubNickname(@RequestBody Map<String, String> githubEmail, @AuthenticationPrincipal CustomUserDetails userDetails){
+        myPageService.updateUserGitNickname(userDetails.getUserId(), githubEmail.get("githubEmail"));
+        return ResponseEntity.ok(CommonResponse.toResponse(CommonCode.OK));
+    }
+}
